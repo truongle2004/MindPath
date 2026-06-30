40155ac8f875e913ef3f3c9e648d326929acd98f
truongle2004 <lesytruong247@gmail.com>
2026-06-30 13:31:49 +0700

    chore: change knip ingore files

--- knip.config.ts
+++ knip.config.ts
@@ -1,22 +1,22 @@
-import type { KnipConfig } from "knip";
+import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
   // Files to exclude from Knip analysis
-  ignore: ["src/libs/I18n.ts", "src/types/I18n.ts"],
-  ignoreFiles: ["src/components/ui/**"],
+  ignore: ['src/libs/I18n.ts', 'src/types/I18n.ts'],
+  ignoreFiles: ['src/components/ui/**'],
   // Dependencies to ignore during analysis
   ignoreDependencies: [
-    "@clerk/shared",
-    "@swc/helpers", // Avoid error in CI: "`npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync."
+    '@clerk/shared',
+    '@swc/helpers', // Avoid error in CI: "`npm ci` can only install packages when your package.json and package-lock.json or npm-shrinkwrap.json are in sync."
   ],
   // Binaries to ignore during analysis
   ignoreBinaries: [],
   // Shadcn UI components export variants and subcomponents for reuse
   ignoreIssues: {
-    "src/components/ui/**": ["exports"],
+    'src/components/ui/**': ['exports'],
   },
   compilers: {
-    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/gu)].join("\n"),
+    css: (text: string) => [...text.matchAll(/(?<=@)import[^;]+/gu)].join('\n'),
   },
   treatConfigHintsAsErrors: true,
 };
